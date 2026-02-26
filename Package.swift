67363203a27dea6e0b902522f2f0fa63c1b27b5b
SungJun Hong <castle_jun@daum.net>
2026-02-26 09:07:28 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 6.2
+
+import CompilerPluginSupport
+import PackageDescription
+
+let package = Package(
+  name: "TCAExtra",
+  platforms: [
+    .iOS(.v16),
+    .macOS(.v13),
+    .tvOS(.v16),
+    .watchOS(.v9),
+  ],
+  products: [
+    .library(
+      name: "TCAExtra",
+      targets: ["TCAExtra"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.24.1"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", from: "602.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "TCAExtra",
+      dependencies: [
+        "TCAExtraMacros",
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+      ]
+    ),
+    .macro(
+      name: "TCAExtraMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+      ]
+    )
+  ]
+)
